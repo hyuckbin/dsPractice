c9797e1bb91d23037723f2925f6f4bea13b4100d
hyuckbin <rpdak159@naver.com>
2025-05-11 18:47:27 +0900

    전역함수

---
+++ ex/01-8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int val = 100; // 전역변수
+
+using std ::cout;
+using std ::endl;
+
+int main(){
+    int val = 20 ;
+    val += 3 ;
+    ::val += 7 ;
+
+    cout << val << endl<< ::val;
+
+    return 0;
+}
