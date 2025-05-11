60c50655da72ee72da5f974da9c35365f06fff0c
hyuckbin <rpdak159@naver.com>
2025-05-11 17:46:37 +0900

    오버로딩 오류

---
+++ ex/01-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int SimpleFunc(int a= 10){
+    return a+1;
+}
+
+// int SimpleFunc(void){
+//     return 10;
+// }
+
+int main(){
+
+    SimpleFunc();
+    
+    return 0;
+}
