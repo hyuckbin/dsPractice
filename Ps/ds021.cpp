c1792a95163e80ee1512b6eaeb1f78f1ff8d4b06
hyuckbin <rpdak159@naver.com>
2025-05-20 23:24:44 +0900

    ADD DS21

---
+++ Ps/ds021.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 3;
+
+class Student{
+    private:
+        double getAvg() const;
+
+    public:
+        string name;
+        string sid;
+        int *score;
+        void print() const;
+};
+
+double Student::getAvg() const{
+    int sum ;
+    double avg;
+
+    for(int i=0; i<SIZE; i++){
+        
+    }
+}
+
+void input(Student &s){
+    cin >> s.sid;
+    for(int i=0; i<SIZE; i++){
+        cin >> s.score[i];
+    }
+    getline(cin,s.name);
+
+}
+int main(){
+    Student s1;
+    s1.score = new int[SIZE];
+
+    input(s1);
+}
