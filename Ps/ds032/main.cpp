732a5dfd1d0c33f096b1dea5915313017b4d0072
hyuckbin <rpdak159@naver.com>
2025-05-22 23:40:15 +0900

    gpa,name 입력받는기능추가

---
+++ Ps/ds032/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string line;
+    string filename;
+    getline(cin, filename); //데이터 파일 이름 입력
+
+    ifstream file(filename);
+    if(!file.is_open()){
+        cerr<< "Failed to open!" << filename << endl;
+        return 1;
+    }
+
+    while(getline(file,line)){
+        stringstream ss(line);
+        double gpa;
+        string name;
+        
+        ss>>gpa>>name;
+
+        cout << "GPA : " << gpa << ", Name: " << name <<endl;
+
+    }
+
+    file.close();
+
+    return 0;
+}
