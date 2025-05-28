a38b09a9922756bfb5549e9e77d449556ba27c5e
hyuckbin <rpdak159@naver.com>
2025-05-28 23:31:55 +0900

    11

--- Ps/ds011.cpp
+++ Ps/ds011.cpp
@@ -4,7 +4,7 @@ using std :: cin ;
 using std :: cout ;
 using std :: endl ;
 
-//숫자들의 총합을 구한다.1
+//숫자들의 총합을 구한다.1a
 int addNum(int count ,int* n){
     int sum = 0;
     for(int i=0; i<count ; i++){
