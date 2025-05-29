036f3e3ccc1b6ed64e8efd5db9c1cda976105245
hyuckbin <rpdak159@naver.com>
2025-05-29 23:02:12 +0900

    1

--- Ps/ds015.cpp
+++ Ps/ds015.cpp
@@ -59,7 +59,7 @@ int main(){
     // cout << s.size()<< endl;
     // cout << s.substr()<< endl;
     // cout << s.substr(2)<< endl;a
-    // cout << s.substr(2,6)<< endl;   // 2부터 6글자 aa
+    // cout << s.substr(2,6)<< endl;   // 2부터 6글자 a
     //sss
     // cout << s.substr(0,3)<< endl;a
     return 0;
