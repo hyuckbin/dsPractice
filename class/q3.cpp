62bf8146fee34be7352df422a60a010f56180707
hyuckbin <rpdak159@naver.com>
2025-05-31 23:32:16 +0900

    1

---
+++ class/q3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class CQ{
+    int front , rear;
+    int maxSize;
+    int *list;
+
+    public:
+        CQ(int size) : front(0),rear(0),maxSize(size+1){
+            list = new int[maxSize]
+        }
+        ~CQ(){
+            delete[] list;
+        }
+        bool isEmpty(){
+            return front == rear;
+        }
+        bool isFull(){
+            return (rear+1) % maxSize == front; 
+        }
+        void enqueue(int val){
+            if(isFull()) return;
+
+            list[front] = val;
+            front = (front +1) &maxSize;
+        }
+        int dequeue(){
+            if(isEmpty()) return
+        }
+        void print();
+        void clear();
+};
+int main(){
+
+    return 0;
+}
